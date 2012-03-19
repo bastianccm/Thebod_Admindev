52c94d4785102c3703b9c31976270621d8d7f246
Bastian Ike <thebod@thebod.de>
2012-03-18 19:46:35 -0700

    small fixes + comments

--- src/app/code/community/Thebod/Admindev/Model/Observer.php
+++ src/app/code/community/Thebod/Admindev/Model/Observer.php
@@ -1,13 +1,23 @@
 <?php
-
+/**
+ * enables adminhtml template hints on adminhtml_block_html_before event
+ *
+ * @author Bastian Ike <thebod@thebod.de>
+ * @license none ;-)
+ */
 class Thebod_Admindev_Model_Observer {
+    /**
+     * set stores/admin/[template hints] values to true, depends on /dev/debug/admin_templateshints
+     *
+     * @param $event
+     */
     public function adminhtmlBlockHtmlBefore($event) {
         if(Mage::getStoreConfig('dev/debug/admin_templatehints')) {
             Mage::getConfig()->setNode('stores/admin/' . Mage_Core_Block_Template::XML_PATH_DEBUG_TEMPLATE_HINTS, 1);
-        }
 
-        if(Mage::getStoreConfig('dev/debug/admin_templatehints_blocks')) {
-            Mage::getConfig()->setNode('stores/admin/' . Mage_Core_Block_Template::XML_PATH_DEBUG_TEMPLATE_HINTS_BLOCKS, 1);
+            if(Mage::getStoreConfig('dev/debug/admin_templatehints_blocks')) {
+                Mage::getConfig()->setNode('stores/admin/' . Mage_Core_Block_Template::XML_PATH_DEBUG_TEMPLATE_HINTS_BLOCKS, 1);
+            }
         }
     }
 }
